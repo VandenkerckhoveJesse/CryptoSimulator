81dd21676d88479e8dcbbf399a0b1e54478255ab
jesse.vandenkerckhove <jesse.vandenkerckhove@student.howest.be>
2022-10-11 01:36:14 +0200

    Added CurrencyService with skeleton code for API

--- src/main/java/be/jessevdk/CryptoSimulator/currency/CurrencyController.java
+++ src/main/java/be/jessevdk/CryptoSimulator/currency/CurrencyController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "api/currency")
@@ -24,7 +25,7 @@ public class CurrencyController {
         return currencyService.getAllCurrencies();
     }
     @GetMapping("{id}")
-    public Currency getCurrency(@PathVariable String id) {
+    public Optional<Currency> getCurrency(@PathVariable String id) {
         return currencyService.getCurrency(id);
     }
 }
--- src/main/java/be/jessevdk/CryptoSimulator/currency/CurrencyService.java
+++ src/main/java/be/jessevdk/CryptoSimulator/currency/CurrencyService.java
@@ -1,28 +1,36 @@
 package be.jessevdk.CryptoSimulator.currency;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CurrencyService {
 
+    private final CurrencyRepository currencyRepository;
+
+    @Autowired
+    public CurrencyService(CurrencyRepository currencyRepository) { this.currencyRepository = currencyRepository; }
+
     public List<Currency> getAllCurrencies() {
-        return List.of(
-                new Currency(
-                        "chacha", "Bitcoin", "25C4"
-                ),
-                new Currency(
-                        "krkao", "Ethereum", "88D9"
-                )
-        );
+        List<Currency> currencies =  currencyRepository.findAll();
+        currencies.stream().forEach(currency -> currency.setPrice(2.5));
+        return currencies;
     }
 
-    public Currency getCurrency(String id) {
-        return new Currency(id, "Bitcoin", "25C4");
+    public Optional<Currency> getCurrency(String id) {
+        var curr = currencyRepository.findById(id);
+        if(curr.isPresent()) {
+            curr.get().setPrice(2.0);
+            return curr;
+        }
+        return currencyRepository.findById(id);
     }
 
 }
