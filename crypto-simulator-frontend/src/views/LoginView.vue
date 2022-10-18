5abc421cf129498d23e5add7eb366ea0b278ecff
jesse.vandenkerckhove <jesse.vandenkerckhove@student.howest.be>
2022-10-18 21:33:30 +0200

    Created login and signup views with necessary vue components

---
+++ crypto-simulator-frontend/src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+    <SignIn></SignIn>
+</template>
+
+<script>
+    import SignIn from "../components/SignIn";
+    export default {
+        name: "LoginView",
+        components: {
+            SignIn,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
