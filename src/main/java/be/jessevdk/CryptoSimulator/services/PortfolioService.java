32beb84140b1db04e2bf982c66b0307c26482a8d
Jesse Vandenkerckhove <jesse.jesse.vdk@gmail.com>
2022-11-07 04:00:15 +0100

    Changed domain models to improve update operations of MongoDB

--- src/main/java/be/jessevdk/CryptoSimulator/services/PortfolioService.java
+++ src/main/java/be/jessevdk/CryptoSimulator/services/PortfolioService.java
@@ -1,5 +1,7 @@
 package be.jessevdk.CryptoSimulator.services;
 
+import be.jessevdk.CryptoSimulator.models.dto.PortfolioDTO;
+import be.jessevdk.CryptoSimulator.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +9,15 @@ import org.springframework.stereotype.Service;
 public class PortfolioService {
     @Autowired
     private CurrencyService currencyService;
+    @Autowired
+    private ApplicationUserService applicationUserService;
+    @Autowired
+    private UserRepository userRepository;
 
-    
+    /*
+    public PortfolioDTO getPortfolio(String username) {
+        Portfolio portfolio = userRepository.findPortfolioByUsername(username);
+        return new PortfolioDTO();
+    }*/
+
 }
