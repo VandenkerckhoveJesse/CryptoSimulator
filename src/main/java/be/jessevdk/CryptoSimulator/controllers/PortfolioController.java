ccf7921e987ef28311bd535d46c4c1619a666feb
Jesse Vandenkerckhove <jesse.jesse.vdk@gmail.com>
2022-11-22 05:01:20 +0100

    Added temporary CrossOrigin permissions

--- src/main/java/be/jessevdk/CryptoSimulator/controllers/PortfolioController.java
+++ src/main/java/be/jessevdk/CryptoSimulator/controllers/PortfolioController.java
@@ -3,6 +3,8 @@ package be.jessevdk.CryptoSimulator.controllers;
 
 import be.jessevdk.CryptoSimulator.models.dto.CoinDTO;
 import be.jessevdk.CryptoSimulator.models.dto.PortfolioDTO;
+import be.jessevdk.CryptoSimulator.models.request.BuyCoinRequest;
+import be.jessevdk.CryptoSimulator.models.request.SellCoinRequest;
 import be.jessevdk.CryptoSimulator.services.PortfolioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,22 +13,25 @@ import java.math.BigDecimal;
 import java.security.Principal;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "api/portfolio")
 public class PortfolioController {
     @Autowired
     PortfolioService portfolioService;
+
     @GetMapping
     public PortfolioDTO getPortfolio(Principal principal) {
         return portfolioService.getPortfolio(principal.getName());
     }
 
+
     @PostMapping(path="coins/buy")
-    public void buyCoin(Principal principal, @RequestParam String id, @RequestParam BigDecimal amount) {
-        portfolioService.buyCoin(principal.getName(), id, amount);
+    public void buyCoin(Principal principal, @RequestBody BuyCoinRequest body) {
+        portfolioService.buyCoin(principal.getName(), body.getId(), body.getAmount());
     }
 
     @PostMapping(path="coins/sell")
-    public void sellCoin(Principal principal, @RequestParam String id, @RequestParam BigDecimal amount) {
-        portfolioService.sellCoin(principal.getName(), id, amount);
+    public void sellCoin(Principal principal, @RequestBody SellCoinRequest body) {
+        portfolioService.sellCoin(principal.getName(), body.getId(), body.getAmount());
     }
 }
